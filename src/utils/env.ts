99cb0763f4963f867075372d330852acb6126f83
Muhammad Noyan Aziz <140343550+atariq-git@users.noreply.github.com>
2026-06-09 13:14:28 +0500

    example env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
-export const env = z
-  .object({
-    NEXT_PUBLIC_API_URL: z.url(),
-  })
-  .parse(process.env);
+const schema = z.object({
+  NEXT_PUBLIC_API_URL: z.url(),
+});
+
+export const env = schema.parse({
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+});
